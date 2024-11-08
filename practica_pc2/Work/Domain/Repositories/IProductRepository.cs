207611dde787dbddffc814d82884da36fc715224
Al3xiel <gabriel280104casas@gmail.com>
2024-11-08 12:48:23 -0500

    feat(interface-repository): added interface repository.

---
+++ practica_pc2/Work/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+using practica_pc2.Shared.Domain.Repositories;
+using practica_pc2.Work.Domain.Model.Aggregates;
+
+namespace practica_pc2.Work.Domain.Repositories;
+
+public interface IProductRepository : IBaseRepository<Product>
+{
+    
+}
