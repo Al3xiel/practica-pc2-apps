c5227206096357bfed9420b5c35292e2e810ce96
Al3xiel <gabriel280104casas@gmail.com>
2024-11-08 12:20:11 -0500

    chore: added program configuration.

--- practica_pc2/Program.cs
+++ practica_pc2/Program.cs
@@ -1,13 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using practica_pc2.Shared.Domain.Repositories;
+using practica_pc2.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+using practica_pc2.Shared.Infrastructure.Persistence.EFC.Configuration;
+using practica_pc2.Shared.Infrastructure.Persistence.EFC.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is null)
+    throw new Exception("Connection string is null.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString);
+});
+
+// OpenAPI/Swagger Configuration
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+//Dependency Injection Configuration
+
+//Shared Bounded Context Dependency Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Work Bounded Context Dependency Injection Configuration
+//TO-DO: Add Dependency Injection Configuration Here Repositories, and Services commands and queries
 
 var app = builder.Build();
+
+// Verify Database Objects are Created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
