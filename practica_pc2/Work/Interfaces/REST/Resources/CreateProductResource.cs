fcdc67727ce0f871045fff12025756450edc0f9d
Al3xiel <gabriel280104casas@gmail.com>
2024-11-08 13:04:09 -0500

    feat(resources): added resources.

---
+++ practica_pc2/Work/Interfaces/REST/Resources/CreateProductResource.cs
@@ -0,0 +1,3 @@
+namespace practica_pc2.Work.Interfaces.REST.Resources;
+
+public record CreateProductResource(string Name, string Description, string Status);
---
+++ practica_pc2/Work/Interfaces/REST/Resources/ProductResource.cs
@@ -0,0 +1,3 @@
+namespace practica_pc2.Work.Interfaces.REST.Resources;
+
+public record ProductResource(int Id, string ProductCode, string Name, string Description, string Status);
